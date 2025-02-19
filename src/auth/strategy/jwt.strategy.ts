c973aa9363c7edc3c4b904968258d554206e9154
pelintopbas <pelin.topbas@eltemtek.com.tr>
2025-02-19 15:56:00 +0300

    jwt.strategy.ts

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -10,9 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         private readonly configService: ConfigService,
         private readonly authService: AuthService,
     ) {
-
         const jwtSecret = configService.get<string>('jwt.secret');
-
         if (!jwtSecret) {
             throw new Error('JWT secret is not defined in the environment variables');
         }
@@ -23,11 +21,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(payload: any) {
-        const user = await this.authService.validateUser(payload.sub);
+
+        const user = await this.authService.validateUserById(payload.sub);
         if (!user) {
             throw new UnauthorizedException();
         }
         return user;
     }
 }
+
 
