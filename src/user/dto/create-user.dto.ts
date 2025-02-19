da03ed727a7c6949d35a33471625f53044ed960c
pelintopbas <pelin.topbas@eltemtek.com.tr>
2025-02-19 10:41:15 +0300

    create-user.dto.ts

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
+import { Role } from 'src/auth/enum/role.enum';
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsString()
+    firstName: string;
+
+    @IsNotEmpty()
+    @IsString()
+    lastName: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+
+    @IsOptional()
+    role?: Role;
+
+    @IsOptional()
+    isActive?: boolean;
+}
