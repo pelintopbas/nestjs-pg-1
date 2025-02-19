e477a53b2b68529f63f740fa4df42eb0b6dca3fc
pelintopbas <pelin.topbas@eltemtek.com.tr>
2025-02-19 15:53:22 +0300

    roles.decorator.ts

--- src/auth/decorators/get-user.decorator.ts
+++ src/auth/decorators/get-user.decorator.ts
@@ -1,8 +1,5 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { SetMetadata } from '@nestjs/common';
+import { Role } from '../enum/role.enum';
 
-export const GetUser = createParamDecorator(
-    (data: unknown, ctx: ExecutionContext) => {
-        const request = ctx.switchToHttp().getRequest();
-        return request.user;
-    },
-);
+export const ROLES_KEY = 'roles';
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
